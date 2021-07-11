fd15e5aa6fc3d3a1f8e1780081fbc2a937eafb27
pyd3690 <prenaamd@gmail.com>
2021-07-11 14:56:42 -0500

    default active link

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -19,21 +19,21 @@ export default function NavBar(props) {
                     alt="React Bootstrap logo"
                 />
             </Navbar.Brand>
-            <Navbar.Brand href="#home" ><span className={styles.title}>Africa Herbal Center</span></Navbar.Brand>
+            <Navbar.Brand href="/" ><span className={styles.title}>Africa Herbal Center</span></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto" activeKey={currentRoute}>
+                <Nav className="mr-auto" defaultActiveKey="/" activeKey={currentRoute}>
                     <Link href="/" passHref>
-                        <Nav.Link href="#Home" className={styles.item}><span className={styles.link} id="home">Accueil</span></Nav.Link>
+                        <Nav.Link href="/" className={styles.item}><span className={styles.link} id="home">Accueil</span></Nav.Link>
                     </Link>
                     <Link href="/store" passHref>
-                        <Nav.Link href="#Store" className={styles.item}><span className={styles.link} id="store">Pharmacopée</span></Nav.Link>
+                        <Nav.Link href="/store" className={styles.item}><span className={styles.link} id="store">Pharmacopée</span></Nav.Link>
                     </Link>
                     <Link href="/blog" passHref>
-                        <Nav.Link href="#Blog" className={styles.item}><span className={styles.link} id="blog">Decouverte</span></Nav.Link>
+                        <Nav.Link href="/blog" className={styles.item}><span className={styles.link} id="blog">Decouverte</span></Nav.Link>
                     </Link>
                     <Link href="/contact" passHref>
-                        <Nav.Link href="#contact" className={styles.item}><span className={styles.link} id="contact">Contact</span></Nav.Link>
+                        <Nav.Link href="/contact" className={styles.item}><span className={styles.link} id="contact">Contact</span></Nav.Link>
                     </Link>
                 </Nav>
 
