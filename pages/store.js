b26d1d5a5a8a3cba8d090bb00c961a3c1a18640c
pyd3690 <prenaamd@gmail.com>
2021-07-12 14:58:18 -0500

    Added pagination to store page

--- pages/store.js
+++ pages/store.js
@@ -43,7 +43,7 @@ export async function getStaticProps() {
     return container;
   })[0] 
 
-  const products = products0.results.slice(0, 3).map(info => {
+  const products = products0.results.map(info => {
     const container = {};
     container['id'] = info.id;
     container['name'] = info.data.name;
