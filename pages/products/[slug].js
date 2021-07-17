645c51b795dabb19526b3f46bad2d786245e6a83
pyd3690 <prenaamd@gmail.com>
2021-07-16 19:15:58 -0500

    added product detail page

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -38,7 +38,7 @@ export async function getStaticPaths() {
     );
 
     const products = products0.results.map(info => (
-        info.slugs[0]
+        info.uid
     ))
     //console.log(products);
     const paths = products.map((productSlug) => ({
@@ -62,7 +62,7 @@ export async function getStaticProps({params}) {
     container['picture'] = info.data.picture.url;
     container['category'] = info.data.category;
     container['price'] = info.data.price;
-    container['slug'] = info.slugs[0];
+    container['slug'] = info.uid;
     container['description'] = info.data.description;
 
     if (container['slug'] === productSlug){
